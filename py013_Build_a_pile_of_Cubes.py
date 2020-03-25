698990939c7f44316d80fcc6a3e33485b15b1af5
zhengxiaocai <46151308+zhengxiaocai@users.noreply.github.com>
2020-03-25 22:14:54 +0800

    Create py013_Build_a_pile_of_Cubes.py

---
+++ py013_Build_a_pile_of_Cubes.py
@@ -0,0 +1,54 @@
+"""
+https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/python
+
+"""
+from math import pow
+
+
+def find_n(m):
+    def f(n):
+        result = []
+        a = 0
+        for i in range(n):
+            a += i ** 3
+            yield a
+        # return result
+    v = int(pow(m, 1/3))
+    b = f(v)
+    print(list(b))
+    if m in b:
+        return list(b).index(m)
+    else:
+        return -1
+
+
+def find_nb(m):
+    sum = 0
+    for i in range(int(pow(m, 1/3))):
+        sum += i ** 3
+        if m == sum:
+            return i
+        elif sum > m:
+            break
+    return -1
+
+
+# def find_nb(m):
+
+
+
+if __name__ == '__main__':
+    print(find_nb(4183059834009), 2022)
+    print(find_nb(24723578342962), -1)
+    print(find_nb(135440716410000), 4824)
+    print(find_nb(40539911473216), 3568)
+    print(find_nb(26825883955641), 3218)
+
+    # print(pow(40539911473216, 1/3))
+    # print(3.0 / 1)
+
+    # print(4183059834009 in [4183059834009, 1])
+
+
+
+
